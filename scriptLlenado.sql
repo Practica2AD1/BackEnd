0895a842c3c38f86fa2069740e7e5ff3e1b241a2
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-23 16:55:14 -0600

    update ScriptLlenado
    Se agregaron las fotos al servicio - 201503609

--- scriptLlenado.sql
+++ scriptLlenado.sql
@@ -29,42 +29,42 @@
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Mantenimiento Periodico','Mantenimiento Periódico de Motor, Frenos, dirección, suspensión, sistemas de enfriamiento, cajas de cambios y transejes.'
-			,400.50,'aun no se',2,1);
+			,400.50,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/mantenimiento_f8cgyp.jpg',2,1);
 	
 	INSERT INTO Servicio(
 		nombreServicio,descripcion,precio, imagen,
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Reparación de Motores a gasolina y diesel','no se que hace esto :v'
-			,400,'aun no se',2,1);
+			,400,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/reparacion_motor_d7qd2n.jpg',2,1);
 	
 	INSERT INTO Servicio(
 		nombreServicio,descripcion,precio, imagen,
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Reparación de sistemas de inyección a gasolina y sistemas electrónicos','no se que hace esto :V'
-			,400.50,'aun no se',2,1);
+			,400.50,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/reparacion_inyectado_oiafo5.jpg',2,1);
 	
 	INSERT INTO Servicio(
 		nombreServicio,descripcion,precio, imagen,
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Reparación de sistemas de calefacción y aire acondicionado automotriz.','saber x3'
-			,400.50,'aun no se',2,1);
+			,400.50,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/reparacion_calefaccion_oox2aa.jpg',2,1);
 
 	INSERT INTO Servicio(
 		nombreServicio,descripcion,precio, imagen,
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Aceite','saber x3'
-			,40,'aun no se',1,1);
+			,40,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/aceite_motor_eguf2r.jpg',1,1);
 
 	INSERT INTO Servicio(
 		nombreServicio,descripcion,precio, imagen,
 		SERVICIO_id_categoria,SERVICIO_id_empresa
 	)
 	VALUES ('Refrigerante','saber x3'
-			,40,'aun no se',1,1);
+			,40,'https://res.cloudinary.com/ddxu7jpgn/image/upload/v1585003915/refrigerante_prestone_ama1gt.jpg',1,1);
 
 /**LLENAR TABLA PREGUNTA **/
 	INSERT INTO Pregunta(
