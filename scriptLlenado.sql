d3201b292ecef1caa779098f00706b7946be96b4
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-23 15:55:49 -0600

    update scriptLlenado
    Se agrego el llenado de la tabla Servicio - 201503609

--- scriptLlenado.sql
+++ scriptLlenado.sql
@@ -1,26 +1,67 @@
 /**LLENAR TABLA EMPRESA**/
-INSERT INTO Empresa(
-telefono, direccion, correo, nombreEmpresa
-)
-VALUES( '24358021','5av B 15-15 Colonia 1ro de Mayo','taller@gmail.com', 'Taller la Bendicion');
+	INSERT INTO Empresa(
+	telefono, direccion, correo, nombreEmpresa
+	)
+	VALUES( '24358021','5av B 15-15 Colonia 1ro de Mayo','taller@gmail.com', 'Taller la Bendicion');
 
 
 /**LLENAR TABLA CategoriaServicio**/
-
-INSERT INTO CategoriaServicio(nombreCategoria)
-VALUES('Producto');
-INSERT INTO CategoriaServicio(nombreCategoria)
-VALUES('Servicio');
+	INSERT INTO CategoriaServicio(nombreCategoria)
+	VALUES('Producto');
+	INSERT INTO CategoriaServicio(nombreCategoria)
+	VALUES('Servicio');
 
 /**LLENAR TABLA CategoriaPregunta**/
-INSERT INTO CategoriaPregunta(nombreCategoria)
-VALUES('Producto');
-INSERT INTO CategoriaPregunta(nombreCategoria)
-VALUES('Servicio');
-INSERT INTO CategoriaPregunta(nombreCategoria)
-VALUES('Falla Mecanica');
-INSERT INTO CategoriaPregunta(nombreCategoria)
-VALUES('Promocion');
-INSERT INTO CategoriaPregunta(nombreCategoria)
-VALUES('Carros');
+	INSERT INTO CategoriaPregunta(nombreCategoria)
+	VALUES('Producto');
+	INSERT INTO CategoriaPregunta(nombreCategoria)
+	VALUES('Servicio');
+	INSERT INTO CategoriaPregunta(nombreCategoria)
+	VALUES('Falla Mecanica');
+	INSERT INTO CategoriaPregunta(nombreCategoria)
+	VALUES('Promocion');
+	INSERT INTO CategoriaPregunta(nombreCategoria)
+	VALUES('Carros');
 
+/**LLENAR TABLA SERVICIO**/
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Mantenimiento Periodico','Mantenimiento Periódico de Motor, Frenos, dirección, suspensión, sistemas de enfriamiento, cajas de cambios y transejes.'
+			,400.50,'aun no se',2,1);
+	
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Reparación de Motores a gasolina y diesel','no se que hace esto :v'
+			,400,'aun no se',2,1);
+	
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Reparación de sistemas de inyección a gasolina y sistemas electrónicos','no se que hace esto :V'
+			,400.50,'aun no se',2,1);
+	
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Reparación de sistemas de calefacción y aire acondicionado automotriz.','saber x3'
+			,400.50,'aun no se',2,1);
+
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Aceite','saber x3'
+			,40,'aun no se',1,1);
+
+	INSERT INTO Servicio(
+		nombreServicio,descripcion,precio, imagen,
+		SERVICIO_id_categoria,SERVICIO_id_empresa
+	)
+	VALUES ('Refrigerante','saber x3'
+			,40,'aun no se',1,1);
