4dd5fb5d2e21eee617b1462990724896bc15f75b
Diego Josue Berrios Gutierrez <diegoberrios15@gmail.com>
2020-03-23 15:44:32 -0600

    update scriptLlenado
    se agrego el llenado de la tabla CategoriaServicio, y CategoriaPregunta - 201503609

--- scriptLlenado.sql
+++ scriptLlenado.sql
@@ -1,4 +1,26 @@
+/**LLENAR TABLA EMPRESA**/
 INSERT INTO Empresa(
 telefono, direccion, correo, nombreEmpresa
 )
 VALUES( '24358021','5av B 15-15 Colonia 1ro de Mayo','taller@gmail.com', 'Taller la Bendicion');
+
+
+/**LLENAR TABLA CategoriaServicio**/
+
+INSERT INTO CategoriaServicio(nombreCategoria)
+VALUES('Producto');
+INSERT INTO CategoriaServicio(nombreCategoria)
+VALUES('Servicio');
+
+/**LLENAR TABLA CategoriaPregunta**/
+INSERT INTO CategoriaPregunta(nombreCategoria)
+VALUES('Producto');
+INSERT INTO CategoriaPregunta(nombreCategoria)
+VALUES('Servicio');
+INSERT INTO CategoriaPregunta(nombreCategoria)
+VALUES('Falla Mecanica');
+INSERT INTO CategoriaPregunta(nombreCategoria)
+VALUES('Promocion');
+INSERT INTO CategoriaPregunta(nombreCategoria)
+VALUES('Carros');
+
